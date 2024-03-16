47911f824134d69e80d34c05787664c4e656d86b
Dialumbrar <156520377+Dialumbrar@users.noreply.github.com>
2024-03-16 08:05:38 -0300

    Atualização

--- cliente/views.py
+++ cliente/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Clientes
 
 def index(request):
@@ -10,3 +10,11 @@ def cadastro(request):
 def listar(request):
     lista_clientes = Clientes.objects.all()
     return render(request, 'listar.html', {'lista': lista_clientes})
+
+def criar(request):
+    nome = request.POST['nome']
+    sobrenome = request.POST['sobrenome']
+    email = request.POST['email']
+    cliente = Clientes.objects.create(primeiro_nome=nome, sobrenome=sobrenome, email=email, status=1)
+    cliente.save()
+    return redirect('index')
